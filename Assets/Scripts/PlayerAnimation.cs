438d3ddb9efda827c95a88061f1b48e63585393e
Pablo Alves <pablorobert@gmail.com>
2022-04-14 00:06:47 -0300

    Running

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -6,6 +6,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     public const int PLAYER_IDLE = 0;
     public const int PLAYER_WALKING = 1;
+    public const int PLAYER_RUNNING = 2;
 
     public const string ANIMATOR_PROPERTY = "transition";
 
@@ -17,7 +18,14 @@ public class PlayerAnimation : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    #region Movement
     void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+    void OnMove()
     {
         if (playerController.Direction.sqrMagnitude > 0)
         {
@@ -37,4 +45,18 @@ public class PlayerAnimation : MonoBehaviour
             transform.eulerAngles = new Vector2(0f, 180f);
         }
     }
+
+    void OnRun()
+    {
+        if (playerController.IsRunning)
+        {
+            anim.SetInteger(ANIMATOR_PROPERTY, PLAYER_RUNNING);
+        }
+        else
+        {
+            anim.SetInteger(ANIMATOR_PROPERTY, PLAYER_IDLE);
+        }
+    }
+
+    #endregion
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private float originalSpeed;
+
     private Rigidbody2D rig;
     private Vector2 direction;
+    public bool IsWalking
+    {
+        get; private set;
+    }
+    private bool isRunning;
 
     public Vector2 Direction
     {
         get { return direction; }
     }
 
+
+    public bool IsRunning
+    {
+        get { return isRunning; }
+    }
+
     void Start()
     {
+        originalSpeed = speed;
         rig = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        OnRun();
+        //OnRun();
     }
     void FixedUpdate()
     {
@@ -39,9 +55,18 @@ public class PlayerController : MonoBehaviour
         );
     }
 
-    void OnRun()
+    void OnRun(bool running)
     {
-
+        if (running)
+        {
+            speed = runSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            speed = originalSpeed;
+            isRunning = false;
+        }
     }
 
     #endregion
@@ -50,15 +75,24 @@ public class PlayerController : MonoBehaviour
 
     public void OnMovement(InputAction.CallbackContext value)
     {
-        /*if (value.canceled)
+        if (value.canceled)
         {
             direction = Vector2.zero;
+            IsWalking = false;
         }
         else
         {
+            IsWalking = true;
             direction = value.ReadValue<Vector2>();
-        }*/
-        direction = value.ReadValue<Vector2>();
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext value)
+    {
+        if (value.started && IsWalking)
+            OnRun(true);
+        if (value.canceled)
+            OnRun(false);
     }
 
     #endregion
