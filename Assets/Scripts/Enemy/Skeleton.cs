1e38ad897d7e77bfc5f33b0c4d84c4663162f127
Pablo Alves <pablorobert@gmail.com>
2022-04-20 10:54:20 -0300

    Enemy with NavMesh2D

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PlayerController playerController;
+    NavMeshAgent agent;
+
+    Animator anim;
+
+    void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(playerController.transform.position);
+
+        if (Vector2.Distance(transform.position, playerController.transform.position) <=
+            agent.stoppingDistance)
+        {
+            //idle
+            PlayAnimation(2);
+        }
+        else
+        {
+            //walking
+            PlayAnimation(1);
+        }
+
+        if (playerController.transform.position.x - transform.position.x > 0)
+        {
+            //right
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+        else
+        {
+            //left
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
+
+    void PlayAnimation(int value)
+    {
+        anim.SetInteger("transition", value);
     }
 }
